c9a36eb090c02bd4c9cbd6c1abe6b5895812b603
tvmolin <thiago.molin@philips.com>
2021-01-09 13:46:06 -0300

    feat: solved problem 4

---
+++ LeetCode/Problems/Problem4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    //Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+    //return the median of the two sorted arrays.
+
+    //Follow up: The overall run time complexity should be O(log (m+n)).
+    public class Problem4
+    {
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] mergedArray = nums1.Concat(nums2).ToArray();
+            if(mergedArray.Length == 1)
+            {
+                return mergedArray[0];
+            }
+            return FindMedian(mergedArray);
+        }
+
+        private static double FindMedian(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int arrayLength = nums.Length;
+
+            //If array has odd amount of items
+            if (arrayLength % 2 == 1)
+            {
+                int medianIndex = (arrayLength / 2);
+                return nums[medianIndex];
+            } else
+            {
+                int medianIndex1 = (arrayLength / 2) - 1;
+                int medianIndex2 = medianIndex1 + 1;
+                return (double) (nums[medianIndex1] + nums[medianIndex2]) / 2;
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Problem4Tests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeTests
+{
+    public class Problem4Tests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 3 }, new int[] { 2 }, 2)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.5)]
+        [InlineData(new int[] { 0, 0 }, new int[] { 0, 0 }, 0)]
+        [InlineData(new int[] { }, new int[] { 1 }, 1)]
+        [InlineData(new int[] { 2 }, new int[] { }, 2)]
+        [InlineData(new int[] { 5, 9, 7, 8, 10 }, new int[] { 11, 25, 32}, 9.5)]
+        public void ShouldReturnMedianOfTwoArrays(int[] nums1, int[] nums2, double expectedValue)
+        {
+            Assert.Equal(expectedValue, Problem4.FindMedianSortedArrays(nums1, nums2));
+        }
+    }
+}
