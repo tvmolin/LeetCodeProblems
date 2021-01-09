2f8738578e3cf0e5c4a2f11a6874f7fb975ee772
tvmolin <thiago.molin@philips.com>
2021-01-09 10:02:16 -0300

    problem 3

---
+++ LeetCode/Problems/Problem3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class Problem3
+    {
+        //Given a string s, 
+        //find the length of the longest substring without repeating characters.
+
+        public static int lengthOfLongestSubstring(string inputString)
+        {
+            if (inputString.Length == 0)
+            {
+                return 0;
+            }
+
+            int longestSoFar = 1;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (i == inputString.Length - 1)
+                {
+                    break;
+                }
+
+                string substring = "" + inputString[i];
+                for (int x = i + 1; x < inputString.Length; x++)
+                {
+                    if (substring.Contains(inputString[x]))
+                    {
+                        break;
+                    }
+                    substring += inputString[x];
+                    longestSoFar = longestSoFar < substring.Length ? substring.Length : longestSoFar;
+                }
+            }
+
+            return longestSoFar;
+        }
+
+    }
+}
---
+++ LeetCodeTests/Problem3Tests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeTests
+{
+    public class Problem3Tests
+    {
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData("", 0)]
+        public void shouldFindLengthOfLongestSubstringWithoutRepeatingCharacters(string input, int expectedOutput)
+        {
+            Assert.Equal(expectedOutput, Problem3.lengthOfLongestSubstring(input));
+        }
+    }
+}
