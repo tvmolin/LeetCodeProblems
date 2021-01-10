7a336b54c0544f9a104b370602f99f635fe12ca4
tvmolin <thiago.molin@philips.com>
2021-01-10 14:07:25 -0300

    feat: Problem 5

---
+++ LeetCode/Problems/Problem5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    //Given a string s, return the longest palindromic substring in s.
+    //"babad", "bab"
+    public class Problem5
+    {
+        public static string LongestPalindromicSubstring(string inputString)
+        {
+            if (inputString.Length == 1)
+            {
+                return inputString;
+            }
+
+            string currentLongest = "";
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                for (int x = inputString.Length; x > i; x--)
+                {
+                    string stringToTest = inputString.Substring(i, x - i);
+                    string reversedString = Reverse(stringToTest);
+                    if ((stringToTest == reversedString) && stringToTest.Length > currentLongest.Length)
+                    {
+                        currentLongest = stringToTest;
+                    }
+                }
+            }
+
+            return currentLongest;
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ LeetCodeTests/Problem5Tests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeTests
+{
+    public class Problem5Tests
+    {
+        [Theory]
+        [InlineData("babad", "bab")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("a", "a")]
+        [InlineData("ac", "a")]
+         public void ShouldReturnLongestPalindrome(string s, string expectedOutput)
+        {
+            Assert.Equal(expectedOutput, Problem5.LongestPalindromicSubstring(s));
+        }
+    }
+}
